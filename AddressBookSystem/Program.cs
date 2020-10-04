ed4387201863a77ed187c63b1de8621665009f18
Shreya0207 <shanu.shreya270@gmail.com>
2020-10-04 19:42:56 +0530

    [Shreya]Add: Creating the Contact

--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -7,6 +7,9 @@ namespace AddressBookSystem
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Address Book Program");
+            AddressBook addressBook = new AddressBook();
+            addressBook.AddContact("Walter", "White", "Bakerstreet", "Raipur", "CG", "heisenberg@gmail.com", 987654, 1234567890);
+            addressBook.ViewContact();
         }
     }
 }
